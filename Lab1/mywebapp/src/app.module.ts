2358bfbf4fd428e5a4ccc9ab2a9d6e20eed1ae3e
inryceu <pavel.maluev@gmail.com>
2026-03-21 18:34:57 +0200

    implemented health check

--- Lab1/mywebapp/src/app.module.ts
+++ Lab1/mywebapp/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NotesModule } from './notes/notes.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [NotesModule],
+  imports: [NotesModule, HealthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ Lab1/mywebapp/src/health/health.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import type { Response } from 'express';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Controller('health')
+export class HealthController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get('alive')
+  getAlive(@Res() res: Response) {
+    return res.status(HttpStatus.OK).send('OK');
+  }
+
+  @Get('ready')
+  async getReady(@Res() res: Response) {
+    try {
+      await this.prisma.$queryRaw`SELECT 1`;
+      return res.status(HttpStatus.OK).send('OK');
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).send('Database connection failed or database is not ready');
+    }
+  }
+}
